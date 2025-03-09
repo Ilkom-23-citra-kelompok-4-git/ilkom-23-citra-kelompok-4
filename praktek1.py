dc291efc3cab6af7ae178dd28672844ab7e4d8da
IankSyahputra <ihramsyih@gmail.com>
2025-03-09 13:50:05 +0800

    udate progres

--- praktek1.py
+++ praktek1.py
@@ -3,12 +3,16 @@ import cv2
 citra = cv2.imread("C:/Users/USER\Documents/Iamfolder/Kuliah/Semester_4/Pengelolaan Citra Digital/Kelompok4-PCD/image/imagekubo.jpg")
 
 # Menampilkan citra digital yang sudah dibaca
-# cv2.imshow("KuboShiori1",citra)
+cv2.imshow("KuboShiori",citra)
+cv2.imshow("KuboShiori-blue",citra[:,:,0]) #menampilkan citra blue
+cv2.imshow("KuboShiori-green",citra[:,:,1]) #menampilkan citra green
+cv2.imshow("KuboShiori-red",citra[:,:,2]) #menampilkan citra red
 
 # Menunngu hingga user menekan sembarang tombol
-# cv2.waitKey()
+cv2.waitKey()
 
 # [:,:] mengambil semua nilai pixel pada citra (baris kolom)
-print("\n=========blue============\n",citra[:,:,0]) #ini adalah chanel blue
-print("\n=========green============\n",citra[:,:,1]) #ini adalah chanel green
-print("\n=========red============\n",citra[:,:,2]) #ini adalah chanel red
+print("\n=========blue============\n",citra[:,:,0]) #menampilkan chanel blue
+print("\n=========green============\n",citra[:,:,1]) #menampilkan chanel green
+print("\n=========red============\n",citra[:,:,2]) #menampilkan chanel red
+print("\n=========semua chanel============\n",citra) #menampilkan semua chanel 
