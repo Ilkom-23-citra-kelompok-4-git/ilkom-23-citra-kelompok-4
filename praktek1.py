c35633e45bdd5f9b34a1871c2a36b020af86a702
IankSyahputra <ihramsyih@gmail.com>
2025-03-09 13:19:14 +0800

    update progres

--- praktek1.py
+++ praktek1.py
@@ -1,3 +1,9 @@
 import cv2
 
-citra = cv2.imread("C:/Users/USER/Documents/Iamfolder/Kuliah/Semester_4/Pengelolaan Citra Digital/Kelompok4-PCD/image/wallhaven-g8qqll.jpg")
+citra = cv2.imread("C:/Users/USER\Documents/Iamfolder/Kuliah/Semester_4/Pengelolaan Citra Digital/Kelompok4-PCD/image/imagekubo.jpg")
+
+# Menampilkan citra digital yang sudah dibaca
+cv2.imshow("KuboShiori1",citra)
+
+# Menunngu hingga user menekan sembarang tombol
+cv2.waitKey()
