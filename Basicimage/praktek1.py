cca0733b62dd7b8c555ab1153e37961dfb7c88ed
IankSyahputra <ihramsyih@gmail.com>
2025-03-10 10:49:23 +0800

    update

--- Basicimage/praktek1.py
+++ Basicimage/praktek1.py
@@ -4,12 +4,9 @@ citra = cv2.imread("C:/Users/USER\Documents/Iamfolder/Kuliah/Semester_4/Pengelol
 
 # Menampilkan citra digital yang sudah dibaca
 cv2.imshow("KuboShiori",citra)
-# cv2.imshow("KuboShiori-blue",citra[:,:,0]) #menampilkan citra blue
-# cv2.imshow("KuboShiori-green",citra[:,:,1]) #menampilkan citra green
-# cv2.imshow("KuboShiori-red",citra[:,:,2]) #menampilkan citra red
-
-citragaus = cv2.GaussianBlur(citra)
-cv2.imshow(citragaus)
+cv2.imshow("KuboShiori-blue",citra[:,:,0]) #menampilkan citra blue
+cv2.imshow("KuboShiori-green",citra[:,:,1]) #menampilkan citra green
+cv2.imshow("KuboShiori-red",citra[:,:,2]) #menampilkan citra red
 
 # [:,:] mengambil semua nilai pixel pada citra (baris kolom)
 print("\n=========blue============\n",citra[:,:,0]) #menampilkan chanel blue
