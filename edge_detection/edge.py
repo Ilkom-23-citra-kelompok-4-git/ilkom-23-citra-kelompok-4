3c9cac4b5401725d847bb1e9efc19908cbf64c2d
IankSyahputra <ihramsyih@gmail.com>
2025-03-27 03:10:17 +0800

    import library and inisialisasi flask

--- edge_detection/edge.py
+++ edge_detection/edge.py
@@ -1,1 +1,8 @@
-from Flask import flask
+import os
+import cv2
+import numpy as np
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
