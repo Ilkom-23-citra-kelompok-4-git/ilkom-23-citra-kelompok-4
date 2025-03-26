5eab1463d9594e05e46421112f79f658dfd9f9e0
IankSyahputra <ihramsyih@gmail.com>
2025-03-27 04:00:37 +0800

    membuat route utama pada flask

--- edge_detection/edge.py
+++ edge_detection/edge.py
@@ -17,3 +17,20 @@ def edge_detection(image_path):
     cv2.imwrite(output_path, edges)
     return output_path
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == "":
+            return redirect(request.url)
+        if file:
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filepath)
+            processed_image = edge_detection(filepath)
+            return render_template("index.html", uploaded_image=file.filename, output_image="output.png")
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
