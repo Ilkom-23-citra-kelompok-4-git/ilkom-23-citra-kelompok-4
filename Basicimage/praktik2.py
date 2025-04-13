a0227176f458af472a3b8e7f69f61a7ddcc73547
rasmaar <rasmaar49@gmail.com>
2025-04-13 12:12:24 +0800

    up

--- Basicimage/praktik2.py
+++ Basicimage/praktik2.py
@@ -14,3 +14,16 @@ median = cv2.medianBlur(img, 9)  # kernel size = 3
 
 # 3. Gaussian Blur
 gaussian = cv2.GaussianBlur(img, (9, 9), 0)  # kernel 3x3, sigmaX = 0 (otomatis)
+
+# Tampilkan semua hasil
+titles = ['Original', 'Mean Filter', 'Median Filter', 'Gaussian Blur']
+images = [img, mean, median, gaussian]
+
+plt.figure(figsize=(12, 6))
+for i in range(4):
+    plt.subplot(1, 4, i+1)
+    plt.imshow(images[i])
+    plt.title(titles[i])
+    plt.axis('off')
+plt.tight_layout()
+plt.show()
