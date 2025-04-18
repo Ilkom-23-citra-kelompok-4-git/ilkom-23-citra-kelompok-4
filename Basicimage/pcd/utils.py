dfd9e4ebac4fbafde7159ba60d121a74792ecb3d
Nanaa813 <alee95514@gmail.com>
2025-04-18 18:01:29 +0800

    add

--- Basicimage/pcd/utils.py
+++ Basicimage/pcd/utils.py
@@ -6,3 +6,9 @@ def load_image(path):
 def convert_to_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+def apply_gaussian_blur(image, ksize=(5, 5)):
+    return cv2.GaussianBlur(image, ksize, 0)
+
+def apply_threshold(image, thresh=127, maxval=255):
+    _, result = cv2.threshold(image, thresh, maxval, cv2.THRESH_BINARY)
+    return result
