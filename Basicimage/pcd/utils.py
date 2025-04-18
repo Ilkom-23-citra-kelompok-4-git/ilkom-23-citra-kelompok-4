d534a248d737f92eb6b8eed2f87e57935eedc400
Nanaa813 <alee95514@gmail.com>
2025-04-18 18:00:28 +0800

    .

--- Basicimage/pcd/utils.py
+++ Basicimage/pcd/utils.py
@@ -5,3 +5,6 @@ def load_image(path):
 
 def convert_to_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def apply_gaussian_blur(image, ksize=(5, 5)):
+    return cv2.GaussianBlur(image, ksize, 0)
