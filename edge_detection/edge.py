442fa61fba1f719f773f3e23bc5a7d5f087fbf46
yasminedwi <yayayyyy25@gmail.com>
2025-04-20 01:26:58 +0800

    + hex

--- edge_detection/edge.py
+++ edge_detection/edge.py
@@ -11,19 +11,23 @@ PROCESSED_FOLDER = 'static/history'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['PROCESSED_FOLDER'] = PROCESSED_FOLDER
 
-def edge_detection(image_path):
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    edges = cv2.Canny(img, 50, 150)
+def edge_detection(image_path, color_hex):
+    img_gray = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    edges = cv2.Canny(img_gray, 50, 150)
 
-    # Extract timestamp from uploaded filename
-    original_filename = os.path.basename(image_path)
-    timestamp = original_filename.split("_")[0]
+    # Konversi warna HEX ke BGR
+    color_bgr = tuple(int(color_hex.lstrip('#')[i:i+2], 16) for i in (4, 2, 0))
 
+    # Convert ke BGR
+    color_edge = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
+    color_edge[edges != 0] = color_bgr
+
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     processed_filename = f"edge_{timestamp}.png"
     processed_path = os.path.join(PROCESSED_FOLDER, processed_filename)
-    cv2.imwrite(processed_path, edges)
+    cv2.imwrite(processed_path, color_edge)
 
-    return original_filename, processed_filename
+    return os.path.basename(image_path), processed_filename
 
 def get_history_images():
     history_folder = "static/history"
@@ -41,12 +45,14 @@ def index():
             return redirect(request.url)
 
         file = request.files['file']
+        color = request.form.get("color", "#fff")
+
         if file:
             timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
             filename = f"{timestamp}_{file.filename}"
             filepath = os.path.join(UPLOAD_FOLDER, filename)
             file.save(filepath)
-            original_filename, processed_filename = edge_detection(filepath)
+            original_filename, processed_filename = edge_detection(filepath, color)
             uploaded_image = f"uploads/{original_filename}"
             output_image = f"history/{processed_filename}"
 
