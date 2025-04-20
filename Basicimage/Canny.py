f40f0162332b9e18bf79b5a7b77cf51cb5666a95
rasmaar <rasmaar49@gmail.com>
2025-04-20 11:02:45 +0800

    gradien

--- Basicimage/Canny.py
+++ Basicimage/Canny.py
@@ -28,3 +28,13 @@ gambar = load_image(path_gambar)
 # 2. TAHAP 1: GAUSSIAN BLUR (PENGHALUSAN)
 # ================================================
 gambar_blur = cv2.GaussianBlur(gambar, (5, 5), 1)  # Sigma = 1
+
+# ================================================
+# 3. TAHAP 2: GRADIEN (OPERATOR SOBEL)
+# ================================================
+gradien_x = cv2.Sobel(gambar_blur, cv2.CV_64F, 1, 0, ksize=3)  # Gradien horizontal
+gradien_y = cv2.Sobel(gambar_blur, cv2.CV_64F, 0, 1, ksize=3)  # Gradien vertikal
+magnitudo = np.sqrt(gradien_x**2 + gradien_y**2)  # Kekuatan tepi
+arah = np.arctan2(gradien_y, gradien_x) * 180/np.pi  # Arah tepi (derajat)
+print("Magnitudo Gradien:", magnitudo[247,805].round(2))
+print("Arah Gradien (derajat):", arah[247,805].round(2))
