48ca6e8e8397d832d83f7b4b26d65b4daff3d0f3
Sechan14 <sechansanjaya692@gmail.com>
2025-04-20 10:09:22 +0700

    update.py

--- Basicimage/pixel.py
+++ Basicimage/pixel.py
@@ -6,3 +6,7 @@ image = cv2.imread("bangunan.jpg", cv2.IMREAD_GRAYSCALE)  # Langsung grayscale
 x, y = 805, 247  # Posisi tengah region
 print(f"=== Region 3x3 di (x={x}, y={y}) ===")
 print("Original Grayscale:\n", image[y-1:y+2, x-1:x+2])
+
+# 2. Gaussian Blur
+blurred = cv2.GaussianBlur(image, (5,5), 1)
+print("\nGaussian Blur:\n", blurred[y-1:y+2, x-1:x+2].round(1))
