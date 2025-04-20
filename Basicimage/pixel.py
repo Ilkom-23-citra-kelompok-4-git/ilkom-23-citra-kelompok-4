a32948a6d35d335fc9090f21688cff9ab6dae624
Sechan14 <sechansanjaya692@gmail.com>
2025-04-20 10:06:43 +0700

    update pixel.py

--- Basicimage/pixel.py
+++ Basicimage/pixel.py
@@ -3,3 +3,6 @@ import numpy as np
 
 # 1. Baca gambar dan pilih region 3x3
 image = cv2.imread("bangunan.jpg", cv2.IMREAD_GRAYSCALE)  # Langsung grayscale
+x, y = 805, 247  # Posisi tengah region
+print(f"=== Region 3x3 di (x={x}, y={y}) ===")
+print("Original Grayscale:\n", image[y-1:y+2, x-1:x+2])
