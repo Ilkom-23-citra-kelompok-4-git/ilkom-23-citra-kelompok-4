42682d0b38a3ed5a53fcf1bd1b28dd3d42ce6abf
Sechan14 <Sechansanjaya692@gmail.com>
2025-04-20 14:00:35 +0700

    pixel(r).py

--- Basicimage/pixel.py
+++ Basicimage/pixel.py
@@ -2,8 +2,17 @@ import cv2
 import numpy as np
 
 # 1. Baca gambar dan pilih region 3x3
-image = cv2.imread("bangunan.jpg", cv2.IMREAD_GRAYSCALE)  # Langsung grayscale
-x, y = 805, 247  # Posisi tengah region
+image_rgb = cv2.imread("bangunan.jpg")  # Baca sebagai BGR
+image = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY) 
+x, y = 806, 271  # Posisi tengah region
+B, G, R = cv2.split(image_rgb)
+# print("Original image:\n", image_rgb[y-1:y+2, x-1:x+2])
+print("Original image:\n")
+print("Nilai B channel:\n", B[y-1:y+2, x-1:x+2])
+print("Nilai G channel:\n", G[y-1:y+2, x-1:x+2])
+print("Nilai R channel:\n", R[y-1:y+2, x-1:x+2])
+
+# image = cv2.imread(image_rgb, cv2.IMREAD_GRAYSCALE)  # Langsung grayscale
 print(f"=== Region 3x3 di (x={x}, y={y}) ===")
 print("Original Grayscale:\n", image[y-1:y+2, x-1:x+2])
 
@@ -27,15 +36,17 @@ def non_max_suppression(mag, angle):
         for j in range(1, mag.shape[1]-1):
             # Tetangga berdasarkan arah gradien
             if angle[i,j] == 0:    # Horizontal
-            elif angle[i,j] == 90: # Vertikal
-               neighbors = [mag[i-1,j], mag[i+1,j]]
                 neighbors = [mag[i,j-1], mag[i,j+1]]
-                else:                  # Diagonal
-                    neighbors = [mag[i-1,j-1], mag[i+1,j+1]] 
-                # Pertahankan hanya nilai maksimum lokal
-                if mag[i,j] >= max(neighbors):
-                    suppressed[i,j] = mag[i,j]
+            elif angle[i,j] == 90: # Vertikal
+                neighbors = [mag[i-1,j], mag[i+1,j]]
+            else:                  # Diagonal
+                neighbors = [mag[i-1,j-1], mag[i+1,j+1]]
+            
+            # Pertahankan hanya nilai maksimum lokal
+            if mag[i,j] >= max(neighbors):
+                suppressed[i,j] = mag[i,j]
     return suppressed
+
 suppressed = non_max_suppression(magnitude, direction)
 print("\nNon-Max Suppression:\n", suppressed[y-1:y+2, x-1:x+2].round(1))
 
