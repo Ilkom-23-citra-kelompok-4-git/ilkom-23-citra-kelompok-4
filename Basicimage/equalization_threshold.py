aa28bf67389cca1f13884c9366d19e8ba90f2ea7
Nanaa813 <alee95514@gmail.com>
2025-06-29 14:29:03 +0800

    mod

--- Basicimage/equalization_threshold.py
+++ Basicimage/equalization_threshold.py
@@ -10,3 +10,5 @@ def print_matrix(citra, x=0, y=0, w=5, h=5, label="Matriks"):
     potongan = citra[y:y+h, x:x+w]
     print(f"\n{label} (dari ({y},{x}), ukuran {h}x{w}):")
     print(np.round(potongan).astype(int))
+
+def cek_koordinat(citra, x, y, label=""):
