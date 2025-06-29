acbd470ab23b7e0fb3f1d68e43952e6df52ca1cf
Nanaa813 <alee95514@gmail.com>
2025-06-29 14:28:24 +0800

    .

--- Basicimage/equalization_threshold.py
+++ Basicimage/equalization_threshold.py
@@ -5,3 +5,8 @@ import matplotlib.pyplot as plt
 # ======================
 # Fungsi Tambahan
 # ======================
+
+def print_matrix(citra, x=0, y=0, w=5, h=5, label="Matriks"):
+    potongan = citra[y:y+h, x:x+w]
+    print(f"\n{label} (dari ({y},{x}), ukuran {h}x{w}):")
+    print(np.round(potongan).astype(int))
