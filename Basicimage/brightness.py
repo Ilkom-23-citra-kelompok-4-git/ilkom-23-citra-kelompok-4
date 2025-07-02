b9b06329523deb4d4418081365ac596b7ab99669
Nanaa813 <alee95514@gmail.com>
2025-07-02 18:49:09 +0800

    3

--- Basicimage/brightness.py
+++ Basicimage/brightness.py
@@ -7,3 +7,7 @@ path = "image/bangunan.jpg"
 gambar = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 if gambar is None:
     raise ValueError("Gambar tidak ditemukan. Cek path dan nama file!")
+
+# ==== Brightness Adjustment ====
+nilai_tambah = 50  # kamu bisa ubah ke -50 juga untuk menggelapkan
+gambar_terang = cv2.add(gambar, nilai_tambah)
