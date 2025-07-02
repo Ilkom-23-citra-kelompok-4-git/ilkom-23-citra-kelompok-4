6c048e6932a9bb7436e6423272133b3c20c9cd2c
Nanaa813 <alee95514@gmail.com>
2025-07-02 18:48:43 +0800

    .

--- Basicimage/brightness.py
+++ Basicimage/brightness.py
@@ -1,3 +1,9 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
+# ==== Load Gambar Grayscale ====
+path = "image/bangunan.jpg"
+gambar = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+if gambar is None:
+    raise ValueError("Gambar tidak ditemukan. Cek path dan nama file!")
