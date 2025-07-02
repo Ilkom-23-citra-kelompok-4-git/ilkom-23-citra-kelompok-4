eef9f0c75a293b5fa60f2e075806c6bc7ef958bc
Nanaa813 <alee95514@gmail.com>
2025-07-02 18:50:00 +0800

    5

--- Basicimage/brightness.py
+++ Basicimage/brightness.py
@@ -18,3 +18,8 @@ plt.subplot(1, 2, 1); plt.imshow(gambar, cmap='gray'); plt.title("Original")
 plt.subplot(1, 2, 2); plt.imshow(gambar_terang, cmap='gray'); plt.title(f"Brightness +{nilai_tambah}")
 plt.tight_layout()
 plt.show()
+
+# ==== Cek Nilai Pixel (Opsional) ====
+y, x = 52, 52
+print(f"Original pixel ({y},{x}):", gambar[y, x])
+print(f"After brightness +{nilai_tambah}:", gambar_terang[y, x])
